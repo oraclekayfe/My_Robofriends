d4ee3292526f1115451d9ab439f1dcd1d7f70058
oraclekayfe <oraclekayfe@gmail.com>
2020-03-28 12:06:46 +0100

    second commit

---
+++ precache-manifest.36a3aa696292adff070855d6591dc381.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62828fc47dbf43fcb3d3e8de97d2e30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d0509983398eedeff50b",
+    "url": "/static/css/main.52f82eed.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0509983398eedeff50b",
+    "url": "/static/js/main.e9d8f171.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
