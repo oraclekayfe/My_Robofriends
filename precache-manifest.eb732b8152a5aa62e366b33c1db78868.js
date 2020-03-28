7365ad0481ef960008f0e2abfc7804c91a3868fc
oraclekayfe <oraclekayfe@gmail.com>
2020-03-28 13:49:36 +0100

    Updates

---
+++ precache-manifest.eb732b8152a5aa62e366b33c1db78868.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d415fcad72b6d6a8433ac9bfa79b59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d5f6af5874587d48e75c",
+    "url": "/static/css/main.52f82eed.chunk.css"
+  },
+  {
+    "revision": "a3a65d4fcbb730942321",
+    "url": "/static/js/2.43071c07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.43071c07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f6af5874587d48e75c",
+    "url": "/static/js/main.8f53e973.chunk.js"
+  },
+  {
+    "revision": "3a172e9daf29f6cc3071",
+    "url": "/static/js/runtime-main.08f98dcf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
